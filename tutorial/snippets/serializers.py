d0e303a89616010c7b792feb629c0e229f6f0f30
Sean Azlin <sazlin@gmail.com>
2014-07-23 10:56:57 -0700

    using hyperlinking between api entities (finished part 5 of tutorial)

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,47 +4,19 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True)
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.Field(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail')
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
-
-
-class SnippetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
-
-    pk = serializers.Field()  # Note: `Field` is an untyped read-only field.
-    title = serializers.CharField(required=False,
-                                  max_length=100)
-    code = serializers.CharField(widget=widgets.Textarea,
-                                 max_length=100000)
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,
-                                       default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES,
-                                    default='friendly')
-    owner = serializers.Field(source='owner.username')
-
-    def restore_object(self, attrs, instance=None):
-        """
-        Create or update a new snippet instance, given a dictionary
-        of deserialized field values.
-
-        Note that if we don't define this method, then deserializing
-        data will simply return a dictionary of items.
-        """
-        if instance:
-            # Update existing instance
-            instance.title = attrs.get('title', instance.title)
-            instance.code = attrs.get('code', instance.code)
-            instance.linenos = attrs.get('linenos', instance.linenos)
-            instance.language = attrs.get('language', instance.language)
-            instance.style = attrs.get('style', instance.style)
-            return instance
-
-        # Create new instance
-        return Snippet(**attrs)
+        fields = ('url', 'username', 'snippets')
